1d3f0fc677f84080a82a33827ebc29377ad1ebd6
placidmasvidal <contact@placidmasvidal.com>
2020-05-15 12:14:40 +0200

    TexturePacker implemented

--- desktop/src/com/placidmasvidal/dodgingheroebycoldwildgames/desktop/DesktopLauncher.java
+++ desktop/src/com/placidmasvidal/dodgingheroebycoldwildgames/desktop/DesktopLauncher.java
@@ -2,10 +2,21 @@ package com.placidmasvidal.dodgingheroebycoldwildgames.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.placidmasvidal.dodgingheroebycoldwildgames.DodgingHero;
 
 public class DesktopLauncher {
+
+	static void pack(){
+		TexturePacker.Settings settings = new TexturePacker.Settings();
+		settings.maxWidth = 2048;
+		settings.maxHeight = 2048;
+		settings.pot = true;
+		TexturePacker.process(settings, "../assets", "packed", "game");
+	}
+
 	public static void main (String[] arg) {
+		pack();
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new DodgingHero(), config);
 	}
